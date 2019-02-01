b8fcd306165bc73d4cb302170f85534f6f4e4734
hadiDanial <hadidanial97@gmail.com>
2019-02-01 23:27:27 +0200

    Edited PathCOllider.cs, example scene
    Added ability to add a PhysicsMaterial2D
    
    Added an example scene of the CalculatePercentByPosition function.

--- PathCreator/Examples/Scripts/PathCollider.cs
+++ PathCreator/Examples/Scripts/PathCollider.cs
@@ -6,13 +6,21 @@ public class PathCollider : MonoBehaviour
 {
     public PathCreator pathCreator;
     public EdgeCollider2D edge;
+    public PhysicsMaterial2D physicsMat;
 
     void Start()
-    {
+        {
         pathCreator = GetComponent<PathCreator>();
+        GenerateCollider();
+        }
+
+    private void GenerateCollider()
+        {
         if (pathCreator != null)
             {
-            edge = gameObject.AddComponent<EdgeCollider2D>();
+            edge = GetComponent<EdgeCollider2D>();
+            if (edge == null)
+                edge = gameObject.AddComponent<EdgeCollider2D>();
             Vector3[] verts = pathCreator.path.vertices;
             Vector2[] verts2D = new Vector2[verts.Length];
             for (int i = 0; i < verts.Length; i++)
@@ -20,6 +28,7 @@ public class PathCollider : MonoBehaviour
                 verts2D[i] = new Vector2(verts[i].x, verts[i].y);
                 }
             edge.points = verts2D;
+            edge.sharedMaterial = physicsMat;
             }
+        }
     }
-}
