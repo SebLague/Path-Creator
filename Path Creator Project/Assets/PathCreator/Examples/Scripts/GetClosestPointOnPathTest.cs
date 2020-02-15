1088c24b3288a4760e211dc39245bca554a72782
Peter Schraut <p-github@console-dev.de>
2020-02-15 17:20:04 +0100

    added test scene/script that checks if the position returned by GetClosestPointOnPath is almost identical to the input position if the input position is on the path already.

---
+++ Path Creator Project/Assets/PathCreator/Examples/Scripts/GetClosestPointOnPathTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace PathCreation.Examples
+{
+	public class GetClosestPointOnPathTest : MonoBehaviour
+	{
+		public PathCreator pathCreator;
+		public Transform objectOnPath;
+
+		void Update()
+		{
+			// Wait for the PathFollower Component to set objectOnPath on the path
+			if (Time.timeSinceLevelLoad < 0.1f)
+				return;
+
+			// Since objectOnPath travels on the path, the actualWorldPoint and closestPointOnPath
+			// should be pretty close to each other.
+			Vector3 actualWorldPoint = objectOnPath.position;
+			Vector3 closestPointOnPath = pathCreator.path.GetClosestPointOnPath(actualWorldPoint);
+
+			var distance = Vector3.Distance(actualWorldPoint, closestPointOnPath);
+			if (distance > 0.001f)
+				Debug.LogErrorFormat(this, "Points too far apart (distance={0})", distance);
+		}
+	}
+}
