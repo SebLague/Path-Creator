c991e34d7a318f036ea589487e2654e5ff6ffc4c
artopivalov <artopivalov@gmail.com>
2021-05-28 17:01:39 +0300

    Normals fix

--- Path Creator Project/Assets/PathCreator/Core/Runtime/Objects/BezierPath.Extensions.cs
+++ Path Creator Project/Assets/PathCreator/Core/Runtime/Objects/BezierPath.Extensions.cs
@@ -62,6 +62,10 @@ namespace PathCreation
         points.Add(lastAnchorSecondControl);
         points.Add(firstAnchorSecondControl);
       }
+      else
+      {
+        perAnchorNormalsAngle.Clear();
+      }
 
       var bezierPath = pathCreator.bezierPath;
       for(int i = 0; i < bezierPath.NumPoints; i++)
